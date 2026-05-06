c5a8c1294f5d23f0e341505f13d3d8756152c273
NasaBoi1234 <wbishop2009@gmail.com>
2026-05-05 19:42:37 -0700

    Added libraries for camera, sti (Simple Tiled Implementation), and windfield (a physics library). Also added a print statement to main.lua to test the commit.

--- main.lua
+++ main.lua
@@ -1,4 +1,15 @@
 function love.load()
+  wf = require "libraries/windfield"
+  world = wf.newWorld(0, 800, false) -- (x gravity, y gravity, sleep)
+
+  camera = require "libraries/camera"
+  cam = camera()
+
+  anim8 = require "libraries/anim8"
+  love.graphics.setDefaultFilter("nearest", "nearest")
+
+  sti = require "libraries/sti"
+  --map = sti("maps/map.lua")
   -- Code (runs when firt boots)
 end
 
