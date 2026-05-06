7c153dbea00d8f68e35be4acb236e2f7cb2617fa
Toasty <117317664+NasaBoi1234@users.noreply.github.com>
2026-05-05 19:25:49 -0700

    Initialize Love2D framework with basic functions
    Add initial Love2D framework functions for loading, updating, and drawing.

---
+++ main.lua
@@ -0,0 +1,11 @@
+function love.load()
+  -- Code (runs when firt boots)
+end
+
+function love.update(dt)
+  -- Code (runs before draw every frame) (dt = delta time)
+end
+
+function love.draw()
+  -- Code (runs after update and is when stuff is drawn to screen)
+end
