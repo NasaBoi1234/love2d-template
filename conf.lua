1e577cebef03cfaa93b804c787fd6a49869624b2
Toasty <117317664+NasaBoi1234@users.noreply.github.com>
2026-05-08 14:16:44 -0700

    Change LÖVE version to 11.5
    Updated LÖVE version to 11.5 for compatibility.

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
   t.title = "Game"
-  t.version = "1.0"
+  t.version = "11.5" --Version of love2d
   t.console = false --Console window for print statements
   t.window.width = 640
   t.window.height = 360 --640x360 is the base pixel-art 16:9 ratio
